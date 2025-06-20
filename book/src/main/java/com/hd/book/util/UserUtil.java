5a97825f6b06f571ca6debfe1c8bbad9ca51af8a
송창용 <thdckdyd123@naver.com>
2025-06-20 19:21:19 +0900

    feat-scy: 유저 정보 받아오는 기능 추가

---
+++ book/src/main/java/com/hd/book/util/UserUtil.java
@@ -0,0 +1,24 @@
+package com.hd.book.util;
+
+import com.hd.book.entity.UserEntity;
+import com.hd.book.repository.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserUtil {
+    private final UserRepository userRepository;
+
+    public UserUtil(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public String getUserEmail() {
+        return SecurityContextHolder.getContext().getAuthentication().getName();
+    }
+
+    public UserEntity getCurrentUser() {
+        return userRepository.findByEmail(getUserEmail())
+                .orElseThrow(() -> new RuntimeException("유저 정보를 찾을 수 없습니다."));
+    }
+}
