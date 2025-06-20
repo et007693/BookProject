8fa77d9aaec272f519e2ba7a6e910b884bea1fa6
Gumraze-git <galaxydh4110@gmail.com>
2025-06-20 17:12:42 +0900

    feat-kdh: JWTAuthenticationFilter 추가 및 요청 필터링 로직 구현
    - `JwtAuthenticationFilter` 클래스 생성
    - Authorization 헤더에서 JWT 토큰 추출 및 유효성 검증 로직 추가
    - 사용자 인증 설정 (`UsernamePasswordAuthenticationToken`) 및 로깅 처리

---
+++ book/src/main/java/com/hd/book/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,46 @@
+package com.hd.book.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+import java.util.Collections;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+                                    throws ServletException, IOException {
+        String header = request.getHeader("Authorization");
+        if (StringUtils.hasText(header) && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+            try {
+                if (jwtUtil.validateToken(token)) {
+                    String username = jwtUtil.getUserName(token);
+                    Authentication auth = new UsernamePasswordAuthenticationToken(
+                            username, null, Collections.emptyList());
+                    ((UsernamePasswordAuthenticationToken) auth)
+                            .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                }
+            } catch (Exception e) {
+                log.error("Cannot set user authentication: {}", e.getMessage());
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
