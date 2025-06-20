810dbab10e2bcbcce98c778065880a3375c67362
Gumraze-git <galaxydh4110@gmail.com>
2025-06-20 17:54:46 +0900

    refactor-kdh: CustomDetailsService 리팩토링 및 불필요한 코드 제거
    - `org.springframework.security.core.userdetails.User` 클래스 직접 사용하도록 수정
    - 불필요한 `List` 임포트 제거 및 코드 가독성 개선

--- book/src/main/java/com/hd/book/service/CustomDetailsService.java
+++ book/src/main/java/com/hd/book/service/CustomDetailsService.java
@@ -3,12 +3,10 @@ package com.hd.book.service;
 import com.hd.book.entity.UserEntity;
 import com.hd.book.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,13 +14,15 @@ public class CustomDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
         UserEntity user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("등록된 사용자가 없습니다. : " + email));
-        return User.builder()
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("등록된 사용자가 없습니다. : " + email));
+
+        return org.springframework.security.core.userdetails.User.builder()
                 .username(user.getEmail())
                 .password(user.getPassword())
-                .authorities(List.of())
                 .build();
     }
 }
