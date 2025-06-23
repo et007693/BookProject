e61b018ba41aae077bfef10fc07e8ca2dd26ec9f
송창용 <thdckdyd123@naver.com>
2025-06-23 17:52:55 +0900

    fix-scy: CORS fix

--- book/src/main/java/com/hd/book/config/SecurityConfig.java
+++ book/src/main/java/com/hd/book/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000")); // 프론트 주소
+        config.setAllowedOrigins(List.of("http://localhost:5173")); // 프론트 주소
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true); // 쿠키, Authorization 헤더 포함 허용
