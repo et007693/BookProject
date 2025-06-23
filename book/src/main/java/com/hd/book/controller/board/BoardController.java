dcb3b59c87c98c4d914e243b31489a7199b56294
송창용 <thdckdyd123@naver.com>
2025-06-23 14:10:23 +0900

    feat-scy: 게시글 수정 기능 추가

--- book/src/main/java/com/hd/book/controller/board/BoardController.java
+++ book/src/main/java/com/hd/book/controller/board/BoardController.java
@@ -34,4 +34,17 @@ public class BoardController {
         }
     }
 
+    @PutMapping("/update/{boardId}")
+    public ResponseEntity<ApiResponseDto<Void>> postBoard(@PathVariable Long boardId, @RequestBody BoardWriteDto dto) {
+        try {
+            boardService.updateBoard(boardId, dto);
+            return ResponseEntity.ok(new ApiResponseDto<>(true, "게시글 수정 성공", null));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest()
+                    .body(new ApiResponseDto<>(false, e.getMessage(), null));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponseDto<>(false, "알 수 없는 오류가 발생했습니다.", null));
+        }
+    }
 }
--- book/src/main/java/com/hd/book/service/BoardService.java
+++ book/src/main/java/com/hd/book/service/BoardService.java
@@ -31,7 +31,7 @@ public class BoardService {
     // 게시글 등록
     public boolean postBoard(BoardWriteDto boardWriteDto) {
         try {
-            // 책정보가 없을 경우를 대비해 조회 or 생성
+            // 책 정보가 없을 경우를 대비해 조회 or 생성
             BookEntity book = bookService.getOrCreateBook(boardWriteDto.getIsbn());
             BoardEntity board = convertDtoToEntity(boardWriteDto);
             // book 연결
@@ -47,6 +47,7 @@ public class BoardService {
     // 게시글 수정
     public boolean updateBoard(Long boardId, BoardWriteDto boardWriteDto) {
         try {
+            BookEntity book = bookService.getOrCreateBook(boardWriteDto.getIsbn());
             BoardEntity board = boardRepository.findById(boardId).
                     orElseThrow(() -> new RuntimeException("해당 게시물이 존재하지 않습니다.."));
             String currentUserEmail = userUtil.getUserEmail();
@@ -55,7 +56,8 @@ public class BoardService {
             board.setType(BoardType.valueOf(boardWriteDto.getType().toUpperCase()));
             board.setTitle(boardWriteDto.getTitle());
             board.setContent(boardWriteDto.getContent());
-//            board.setImage(boardWriteDto.getImage());
+            board.setBook(book);
+            // board.setImage(boardWriteDto.getImage());
             boardRepository.save(board);
             return true;
 
@@ -68,13 +70,12 @@ public class BoardService {
     // DTO -> Entity 변환
      private BoardEntity convertDtoToEntity(BoardWriteDto boardWriteDto) {
          UserEntity user = userUtil.getUser();
-         BookEntity book = bookUtil.getBookByIsbn(boardWriteDto.getIsbn());
 
          BoardEntity board = new BoardEntity();
          board.setType(BoardType.valueOf(boardWriteDto.getType().toUpperCase()));
          board.setTitle(boardWriteDto.getTitle());
          board.setContent(boardWriteDto.getContent());
-//         board.setImage(boardWriteDto.getImage());
+         // board.setImage(boardWriteDto.getImage());
          board.setUser(user);
          return board;
      }
