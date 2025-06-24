04750b1a07a33e2cb0dfcea0bd16abdb5cf24dcb
Gumraze-git <galaxydh4110@gmail.com>
2025-06-24 21:54:15 +0900

    feat-kdh: `HistoryRepository`에 사용자와 도서 존재 여부 확인 메서드 추가
    - `existsByUserAndBook` 메서드 구현 for 유효성 검사 및 중복 체크

--- book/src/main/java/com/hd/book/repository/HistoryRepository.java
+++ book/src/main/java/com/hd/book/repository/HistoryRepository.java
@@ -1,7 +1,9 @@
 package com.hd.book.repository;
 
 import com.hd.book.constant.HistoryStatus;
+import com.hd.book.entity.BookEntity;
 import com.hd.book.entity.HistoryEntity;
+import com.hd.book.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -17,4 +19,6 @@ public interface HistoryRepository extends JpaRepository<HistoryEntity, Long> {
 
     // 상태별 조회
     List<HistoryEntity> findByUserUserIdAndStatus(Long UserId, String status);
+
+    boolean existsByUserAndBook(UserEntity user, BookEntity book);
 }
