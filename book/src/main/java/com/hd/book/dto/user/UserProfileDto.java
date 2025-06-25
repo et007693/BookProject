8972e6a62a803ae009d43567a69b72d078d05125
Gumraze-git <galaxydh4110@gmail.com>
2025-06-25 13:39:39 +0900

    fix-kdh: `UserProfileDto`에서 불필요한 import 제거 및 Boolean 기본값 변경
    - Boolean이 `null`값을 갖지 않도록 수정

--- book/src/main/java/com/hd/book/dto/user/UserProfileDto.java
+++ book/src/main/java/com/hd/book/dto/user/UserProfileDto.java
@@ -11,23 +11,7 @@ public class UserProfileDto {
     private final String email;
     private final String bio;
     private final String profileImage;
-    private final Boolean readHistoryVisible;
+    @Builder.Default
+    private final Boolean readHistoryVisible = true;
     private final String createdAt;
-
-    @Builder
-    public UserProfileDto(Long userId,
-                          String nickname,
-                          String email,
-                          String bio,
-                          String profileImage,
-                          Boolean readHistoryVisible,
-                          String createdAt) {
-        this.userId = userId;
-        this.nickname = nickname;
-        this.email = email;
-        this.bio = bio;
-        this.profileImage = profileImage;
-        this.readHistoryVisible = readHistoryVisible;          // Boolean 이므로 null 허용
-        this.createdAt = createdAt;
-    }
 }
