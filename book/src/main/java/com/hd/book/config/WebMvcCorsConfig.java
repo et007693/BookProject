6254504a73cc93ec3b501582fdc1c5de78567715
Gumraze-git <galaxydh4110@gmail.com>
2025-06-26 15:19:14 +0900

    feat-kdh: Spring `WebMvcConfigurer` 기반 CORS 설정 추가
    - `/src/main/java/com/hd/book/config/WebMvcCorsConfig.java` 생성
    - 특정 Origin 및 모든 HTTP 메서드, 헤더, Credentials 허용

---
+++ book/src/main/java/com/hd/book/config/WebMvcCorsConfig.java
@@ -0,0 +1,17 @@
+package com.hd.book.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcCorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:5173")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
