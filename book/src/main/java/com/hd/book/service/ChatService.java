103a0390eab22297923b090ab944cf5b11846ee2
송창용 <thdckdyd123@naver.com>
2025-07-07 11:32:51 +0900

    fix-scy: 유저 nickname 추가

--- book/src/main/java/com/hd/book/service/ChatService.java
+++ book/src/main/java/com/hd/book/service/ChatService.java
@@ -1,9 +1,11 @@
 package com.hd.book.service;
 
-import com.hd.book.dto.chat.ChatMessage;
+import com.hd.book.dto.chat.ChatMessageDto;
 import com.hd.book.entity.ChatMessageEntity;
 import com.hd.book.exception.ChatException;
 import com.hd.book.repository.ChatMessageRepository;
+import com.hd.book.util.UserUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataAccessException;
 import org.springframework.messaging.MessagingException;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -13,19 +15,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class ChatService {
 
     private final SimpMessagingTemplate messagingTemplate;
     private final ChatMessageRepository chatMessageRepository;
-
-    public ChatService(SimpMessagingTemplate messagingTemplate,
-                       ChatMessageRepository chatMessageRepository) {
-        this.messagingTemplate = messagingTemplate;
-        this.chatMessageRepository = chatMessageRepository;
-    }
+    private final UserUtil userUtil;
 
     // 채팅 메시지를 저장하고, 전체 구독자에게 브로드캐스트합니다.
-    public void sendMessage(ChatMessage dto) {
+    public void sendMessage(ChatMessageDto dto) {
         if (dto.getContent() == null || dto.getContent().trim().isEmpty()) {
             throw new ChatException("메시지 내용이 비어 있습니다.");
         }
@@ -34,6 +32,7 @@ public class ChatService {
 
         ChatMessageEntity entity = ChatMessageEntity.builder()
                 .sender(dto.getSender())
+                .nickname(userUtil.getUser().getNickname())
                 .content(dto.getContent())
                 .timestamp(dto.getTimestamp())
                 .build();
@@ -50,11 +49,12 @@ public class ChatService {
         }
     }
 
-    public List<ChatMessage> getHistory() {
+    public List<ChatMessageDto> getHistory() {
         try {
             return chatMessageRepository.findAllByOrderByTimestampAsc().stream()
-                    .map(e -> ChatMessage.builder()
+                    .map(e -> ChatMessageDto.builder()
                             .sender(e.getSender())
+                            .nickname(e.getNickname())
                             .content(e.getContent())
                             .timestamp(e.getTimestamp())
                             .build())
